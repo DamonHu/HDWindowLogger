00315359626877d113421a965393091d011079a8
Damon <dong765@qq.com>
2019-05-28 15:58:58 +0800

    Demo示例
    Demo示例

--- HDWindowLogger/ViewController.m
+++ HDWindowLogger/ViewController.m
@@ -34,6 +34,16 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         HDNormalLog(@"sdfsdfsdfsdfsdf收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方");
     });
+    
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    button.backgroundColor = [UIColor redColor];
+    [button addTarget:self action:@selector(p_click) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
 }
 
+- (void)p_click {
+    HDWarnLog(@"点击按钮");
+    NSLog(@"点击了按钮");
+}
 @end
