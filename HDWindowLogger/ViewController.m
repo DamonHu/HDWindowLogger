592634debfd0d104f52f29867829d13842c8429c
Damon <dong765@qq.com>
2019-06-18 09:49:57 +0800

    demo

--- HDWindowLogger/ViewController.m
+++ HDWindowLogger/ViewController.m
@@ -32,10 +32,12 @@
     });
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        HDWarnLog(@"sdfsdfsdfsdfsdf收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方");
+        HDWarnLog(@"收到就好丰盛的金凤凰就水电费交mmm换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方");
     });
     
-    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        HDWarnLog(@"nnnnnnnnnnnn");
+    });
     UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
     button.backgroundColor = [UIColor redColor];
     [button addTarget:self action:@selector(p_click) forControlEvents:UIControlEventTouchUpInside];
