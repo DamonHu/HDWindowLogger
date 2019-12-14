60e1b478dce51993582701d1c2d0dad242e5fd1e
Damon <dong765@qq.com>
2019-12-14 10:13:24 +0800

    U 示例代码更新
    U 示例代码更新

--- HDWindowLogger/ViewController.m
+++ HDWindowLogger/ViewController.m
@@ -23,8 +23,8 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
+    [HDWindowLogger defaultWindowLogger].mCompleteLogOut = true;
     [HDWindowLogger show];
-   
     HDNormalLog(@"收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方收到就好丰盛的金凤凰就水电费交换机时间开始的回复是砍价的回复四大皆空回复速度快解放后搜的飞机开会接口是点击开发还是手机打开混分巨兽地方");
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
@@ -46,12 +46,10 @@
 }
 
 - (void)p_click {
-    HDWarnLog(@"点击按钮");
-    NSLog(@"点击了按钮");
-    [HDWindowLogger printLog:@"点击按钮" withLogType:kHDLogTypeWarn];
-    NSDictionary *dic = @{@"hhhhhhh":@"撒旦法是打发斯蒂芬是打发斯蒂芬"};
-    [HDWindowLogger printLog:dic withLogType:kHDLogTypeNormal];
+    HDErrorLog(@"点击按钮");
+    NSDictionary *dic = @{@"hhhhhhh":@"撒旦法是打发斯蒂芬是打发斯蒂芬",@"2222":@"更多内容"};
+    HDNormalLog(dic);
     NSArray *array = @[@"2323232323",@"6666678798778",@"00000000"];
-    [HDWindowLogger printLog:array withLogType:kHDLogTypeNormal];
+    HDWarnLog(array);
 }
 @end
